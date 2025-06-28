da57879a3a42eace8141b0f18e3684848018b8db
Ubuntu <ubuntu@ip-172-31-36-229.us-west-2.compute.internal>
2025-06-28 00:07:46 +0000

    adding range in main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
   region  = "us-west-2"
-  profile = "Kaustubh" # Add your profile for this region
+  profile = "Kaustubh"
 }
 
 provider "aws" {
   alias   = "useast1"
   region  = "us-east-1"
-  profile = "Kaustubh" # Use the same profile or another if different
+  profile = "Kaustubh"
 }
 
 provider "aws" {
   alias   = "apac"
   region  = "ap-southeast-1"
-  profile = "Kaustubh" # Ensure this profile exists in your AWS credentials
+  profile = "Kaustubh"
 }
 
 locals {
@@ -20,11 +20,36 @@ locals {
   cpu_threshold_writer        = 80
   connection_threshold_reader = 1800
   connection_threshold_writer = 2100
+
+  rds_clusters = [
+    for i in range(1, 5) : {
+      name   = "west-${format("%02d", i)}"
+      reader = "west-${format("%02d", i)}-reader-db"
+      writer = "west-${format("%02d", i)}-writer-db"
+    }
+  ]
+
+  rds_clusters_east = [
+    for i in range(1, 5) : {
+      name   = "east-${format("%02d", i)}"
+      reader = "east-${format("%02d", i)}-reader-db"
+      writer = "east-${format("%02d", i)}-writer-db"
+    }
+  ]
+
+  rds_clusters_apac = [
+    for i in range(1, 26) : {
+      name   = "apac-${format("%02d", i)}"
+      reader = "kp-apac-instance-${format("%02d", i)}-Reader"
+      writer = "kp-apac-instance-${format("%02d", i)}-Writer"
+    }
+  ]
 }
 
+# WEST REGION MODULES (default provider)
 module "rds_alarms" {
   for_each = {
-    for item in var.rds_clusters : "${item.name}_READER" => {
+    for item in local.rds_clusters : "${item.name}_READER" => {
       db_id          = item.reader
       alarm_prefix   = "New-${item.name}-RDS-READER"
       cpu_threshold  = local.cpu_threshold_reader
@@ -41,7 +66,7 @@ module "rds_alarms" {
 
 module "rds_writer_alarms" {
   for_each = {
-    for item in var.rds_clusters : "${item.name}_WRITER" => {
+    for item in local.rds_clusters : "${item.name}_WRITER" => {
       db_id          = item.writer
       alarm_prefix   = "New-${item.name}-RDS-WRITER"
       cpu_threshold  = local.cpu_threshold_writer
@@ -56,9 +81,10 @@ module "rds_writer_alarms" {
   connection_threshold   = each.value.conn_threshold
 }
 
+# EAST REGION MODULES
 module "rds_alarms_east" {
   for_each = {
-    for item in var.rds_clusters_east : "${item.name}_READER" => {
+    for item in local.rds_clusters_east : "${item.name}_READER" => {
       db_id          = item.reader
       alarm_prefix   = "New-${item.name}-RDS-READER"
       cpu_threshold  = local.cpu_threshold_reader
@@ -80,7 +106,7 @@ module "rds_alarms_east" {
 
 module "rds_writer_alarms_east" {
   for_each = {
-    for item in var.rds_clusters_east : "${item.name}_WRITER" => {
+    for item in local.rds_clusters_east : "${item.name}_WRITER" => {
       db_id          = item.writer
       alarm_prefix   = "New-${item.name}-RDS-WRITER"
       cpu_threshold  = local.cpu_threshold_writer
@@ -100,9 +126,10 @@ module "rds_writer_alarms_east" {
   connection_threshold   = each.value.conn_threshold
 }
 
+# APAC REGION MODULES
 module "rds_alarms_apac" {
   for_each = {
-    for item in var.rds_clusters_apac : "${item.name}_READER" => {
+    for item in local.rds_clusters_apac : "${item.name}_READER" => {
       db_id          = item.reader
       alarm_prefix   = "New-${item.name}-RDS-READER"
       cpu_threshold  = local.cpu_threshold_reader
@@ -124,7 +151,7 @@ module "rds_alarms_apac" {
 
 module "rds_writer_alarms_apac" {
   for_each = {
-    for item in var.rds_clusters_apac : "${item.name}_WRITER" => {
+    for item in local.rds_clusters_apac : "${item.name}_WRITER" => {
       db_id          = item.writer
       alarm_prefix   = "New-${item.name}-RDS-WRITER"
       cpu_threshold  = local.cpu_threshold_writer
@@ -144,6 +171,7 @@ module "rds_writer_alarms_apac" {
   connection_threshold   = each.value.conn_threshold
 }
 
+# SQS QUEUE ALARMS (West Region - default provider)
 module "sqs_alarms" {
   source = "./modules/sqs_alarms"
 
@@ -151,11 +179,12 @@ module "sqs_alarms" {
     for queue in var.sqs_queues : queue.name => queue
   }
 
-  queue_name                   = each.value.name
-  visible_threshold            = each.value.visible_threshold
+  queue_name                    = each.value.name
+  visible_threshold             = each.value.visible_threshold
   oldest_message_age_threshold = each.value.oldest_message_age_threshold
 }
 
+# SQS QUEUE ALARMS (East Region - useast1 provider)
 module "sqs_alarms_east" {
   source = "./modules/sqs_alarms"
 
@@ -167,23 +196,7 @@ module "sqs_alarms_east" {
     for queue in var.sqs_queues_east : queue.name => queue
   }
 
-  queue_name                   = each.value.name
-  visible_threshold            = each.value.visible_threshold
-  oldest_message_age_threshold = each.value.oldest_message_age_threshold
-}
-
-module "sqs_alarms_apac" {
-  source = "./modules/sqs_alarms"
-
-  providers = {
-    aws = aws.apac
-  }
-
-  for_each = {
-    for queue in var.sqs_queues_apac : queue.name => queue
-  }
-
-  queue_name                   = each.value.name
-  visible_threshold            = each.value.visible_threshold
+  queue_name                    = each.value.name
+  visible_threshold             = each.value.visible_threshold
   oldest_message_age_threshold = each.value.oldest_message_age_threshold
 }
