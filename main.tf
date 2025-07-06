12fc1194043e12a7a1c04b695756cac660c5d53e
Ubuntu <ubuntu@ip-172-31-36-229.us-west-2.compute.internal>
2025-07-06 03:31:19 +0000

    Region specific changes in Alarms

--- main.tf
+++ main.tf
@@ -16,8 +16,14 @@ provider "aws" {
 }
 
 locals {
+  # Region-specific alarm configuration
+  west_config = var.alarm_config_by_region["us-west-2"]
+  east_config = var.alarm_config_by_region["us-east-1"]
+  apac_config = var.alarm_config_by_region["ap-southeast-1"]
+
+  # RDS clusters by region
   rds_clusters = [
-    for i in range(1, 5) : {
+    for i in range(1, 25) : {
       name   = "west-${format("%02d", i)}"
       reader = "west-${format("%02d", i)}-reader-db"
       writer = "west-${format("%02d", i)}-writer-db"
@@ -25,7 +31,7 @@ locals {
   ]
 
   rds_clusters_east = [
-    for i in range(1, 5) : {
+    for i in range(1, 25) : {
       name   = "east-${format("%02d", i)}"
       reader = "east-${format("%02d", i)}-reader-db"
       writer = "east-${format("%02d", i)}-writer-db"
@@ -33,7 +39,7 @@ locals {
   ]
 
   rds_clusters_apac = [
-    for i in range(1, 26) : {
+    for i in range(1, 25) : {
       name   = "apac-${format("%02d", i)}"
       reader = "kp-apac-instance-${format("%02d", i)}-Reader"
       writer = "kp-apac-instance-${format("%02d", i)}-Writer"
@@ -41,163 +47,151 @@ locals {
   ]
 }
 
+######################################
 # RDS ALARMS - WEST REGION
-module "rds_alarms" {
+######################################
+
+module "rds_alarms_west" {
   source = "./modules/rds_alarm"
 
   for_each = {
     for item in local.rds_clusters : "${item.name}_READER" => {
-      db_id          = item.reader
-      alarm_prefix   = "New-${item.name}-RDS-READER"
-      cpu_threshold  = var.alarm_config.cpu_threshold_reader
-      conn_threshold = var.alarm_config.connection_threshold_reader
+      db_id        = item.reader
+      alarm_prefix = "New-${item.name}-RDS-READER"
     }
   }
 
   alarm_prefix           = each.value.alarm_prefix
   db_instance_identifier = each.value.db_id
-  cpu_threshold          = each.value.cpu_threshold
-  connection_threshold   = each.value.conn_threshold
-  period                 = var.alarm_config.period
-  evaluation_periods     = var.alarm_config.evaluation_periods
-  datapoints_to_alarm    = var.alarm_config.datapoints_to_alarm
-  statistic              = var.alarm_config.statistic
+  cpu_threshold          = local.west_config.cpu_threshold_reader
+  connection_threshold   = local.west_config.connection_threshold_reader
+  period                 = local.west_config.period
+  evaluation_periods     = local.west_config.evaluation_periods
+  datapoints_to_alarm    = local.west_config.datapoints_to_alarm
+  statistic              = local.west_config.statistic
 }
 
-module "rds_writer_alarms" {
+module "rds_writer_alarms_west" {
   source = "./modules/rds_alarm"
 
   for_each = {
     for item in local.rds_clusters : "${item.name}_WRITER" => {
-      db_id          = item.writer
-      alarm_prefix   = "New-${item.name}-RDS-WRITER"
-      cpu_threshold  = var.alarm_config.cpu_threshold_writer
-      conn_threshold = var.alarm_config.connection_threshold_writer
+      db_id        = item.writer
+      alarm_prefix = "New-${item.name}-RDS-WRITER"
     }
   }
 
   alarm_prefix           = each.value.alarm_prefix
   db_instance_identifier = each.value.db_id
-  cpu_threshold          = each.value.cpu_threshold
-  connection_threshold   = each.value.conn_threshold
-  period                 = var.alarm_config.period
-  evaluation_periods     = var.alarm_config.evaluation_periods
-  datapoints_to_alarm    = var.alarm_config.datapoints_to_alarm
-  statistic              = var.alarm_config.statistic
+  cpu_threshold          = local.west_config.cpu_threshold_writer
+  connection_threshold   = local.west_config.connection_threshold_writer
+  period                 = local.west_config.period
+  evaluation_periods     = local.west_config.evaluation_periods
+  datapoints_to_alarm    = local.west_config.datapoints_to_alarm
+  statistic              = local.west_config.statistic
 }
 
+######################################
 # RDS ALARMS - EAST REGION
-module "rds_alarms_east" {
-  source = "./modules/rds_alarm"
+######################################
 
-  providers = {
-    aws = aws.useast1
-  }
+module "rds_alarms_east" {
+  source    = "./modules/rds_alarm"
+  providers = { aws = aws.useast1 }
 
   for_each = {
     for item in local.rds_clusters_east : "${item.name}_READER" => {
-      db_id          = item.reader
-      alarm_prefix   = "New-${item.name}-RDS-READER"
-      cpu_threshold  = var.alarm_config.cpu_threshold_reader
-      conn_threshold = var.alarm_config.connection_threshold_reader
+      db_id        = item.reader
+      alarm_prefix = "New-${item.name}-RDS-READER"
     }
   }
 
   alarm_prefix           = each.value.alarm_prefix
   db_instance_identifier = each.value.db_id
-  cpu_threshold          = each.value.cpu_threshold
-  connection_threshold   = each.value.conn_threshold
-  period                 = var.alarm_config.period
-  evaluation_periods     = var.alarm_config.evaluation_periods
-  datapoints_to_alarm    = var.alarm_config.datapoints_to_alarm
-  statistic              = var.alarm_config.statistic
+  cpu_threshold          = local.east_config.cpu_threshold_reader
+  connection_threshold   = local.east_config.connection_threshold_reader
+  period                 = local.east_config.period
+  evaluation_periods     = local.east_config.evaluation_periods
+  datapoints_to_alarm    = local.east_config.datapoints_to_alarm
+  statistic              = local.east_config.statistic
 }
 
 module "rds_writer_alarms_east" {
-  source = "./modules/rds_alarm"
-
-  providers = {
-    aws = aws.useast1
-  }
+  source    = "./modules/rds_alarm"
+  providers = { aws = aws.useast1 }
 
   for_each = {
     for item in local.rds_clusters_east : "${item.name}_WRITER" => {
-      db_id          = item.writer
-      alarm_prefix   = "New-${item.name}-RDS-WRITER"
-      cpu_threshold  = var.alarm_config.cpu_threshold_writer
-      conn_threshold = var.alarm_config.connection_threshold_writer
+      db_id        = item.writer
+      alarm_prefix = "New-${item.name}-RDS-WRITER"
     }
   }
 
   alarm_prefix           = each.value.alarm_prefix
   db_instance_identifier = each.value.db_id
-  cpu_threshold          = each.value.cpu_threshold
-  connection_threshold   = each.value.conn_threshold
-  period                 = var.alarm_config.period
-  evaluation_periods     = var.alarm_config.evaluation_periods
-  datapoints_to_alarm    = var.alarm_config.datapoints_to_alarm
-  statistic              = var.alarm_config.statistic
+  cpu_threshold          = local.east_config.cpu_threshold_writer
+  connection_threshold   = local.east_config.connection_threshold_writer
+  period                 = local.east_config.period
+  evaluation_periods     = local.east_config.evaluation_periods
+  datapoints_to_alarm    = local.east_config.datapoints_to_alarm
+  statistic              = local.east_config.statistic
 }
 
+######################################
 # RDS ALARMS - APAC REGION
-module "rds_alarms_apac" {
-  source = "./modules/rds_alarm"
+######################################
 
-  providers = {
-    aws = aws.apac
-  }
+module "rds_alarms_apac" {
+  source    = "./modules/rds_alarm"
+  providers = { aws = aws.apac }
 
   for_each = {
     for item in local.rds_clusters_apac : "${item.name}_READER" => {
-      db_id          = item.reader
-      alarm_prefix   = "New-${item.name}-RDS-READER"
-      cpu_threshold  = var.alarm_config.cpu_threshold_reader
-      conn_threshold = var.alarm_config.connection_threshold_reader
+      db_id        = item.reader
+      alarm_prefix = "New-${item.name}-RDS-READER"
     }
   }
 
   alarm_prefix           = each.value.alarm_prefix
   db_instance_identifier = each.value.db_id
-  cpu_threshold          = each.value.cpu_threshold
-  connection_threshold   = each.value.conn_threshold
-  period                 = var.alarm_config.period
-  evaluation_periods     = var.alarm_config.evaluation_periods
-  datapoints_to_alarm    = var.alarm_config.datapoints_to_alarm
-  statistic              = var.alarm_config.statistic
+  cpu_threshold          = local.apac_config.cpu_threshold_reader
+  connection_threshold   = local.apac_config.connection_threshold_reader
+  period                 = local.apac_config.period
+  evaluation_periods     = local.apac_config.evaluation_periods
+  datapoints_to_alarm    = local.apac_config.datapoints_to_alarm
+  statistic              = local.apac_config.statistic
 }
 
 module "rds_writer_alarms_apac" {
-  source = "./modules/rds_alarm"
-
-  providers = {
-    aws = aws.apac
-  }
+  source    = "./modules/rds_alarm"
+  providers = { aws = aws.apac }
 
   for_each = {
     for item in local.rds_clusters_apac : "${item.name}_WRITER" => {
-      db_id          = item.writer
-      alarm_prefix   = "New-${item.name}-RDS-WRITER"
-      cpu_threshold  = var.alarm_config.cpu_threshold_writer
-      conn_threshold = var.alarm_config.connection_threshold_writer
+      db_id        = item.writer
+      alarm_prefix = "New-${item.name}-RDS-WRITER"
     }
   }
 
   alarm_prefix           = each.value.alarm_prefix
   db_instance_identifier = each.value.db_id
-  cpu_threshold          = each.value.cpu_threshold
-  connection_threshold   = each.value.conn_threshold
-  period                 = var.alarm_config.period
-  evaluation_periods     = var.alarm_config.evaluation_periods
-  datapoints_to_alarm    = var.alarm_config.datapoints_to_alarm
-  statistic              = var.alarm_config.statistic
+  cpu_threshold          = local.apac_config.cpu_threshold_writer
+  connection_threshold   = local.apac_config.connection_threshold_writer
+  period                 = local.apac_config.period
+  evaluation_periods     = local.apac_config.evaluation_periods
+  datapoints_to_alarm    = local.apac_config.datapoints_to_alarm
+  statistic              = local.apac_config.statistic
 }
 
-# SQS QUEUE ALARMS - WEST
-module "sqs_alarms" {
+######################################
+# SQS QUEUE ALARMS - WEST REGION
+######################################
+
+module "sqs_alarms_west" {
   source = "./modules/sqs_alarms"
 
   for_each = {
-    for queue in var.sqs_queues : queue.name => queue
+    for queue in var.sqs_queues_west : queue.name => queue
   }
 
   queue_name                   = each.value.name
@@ -205,16 +199,37 @@ module "sqs_alarms" {
   oldest_message_age_threshold = each.value.oldest_message_age_threshold
 }
 
-# SQS QUEUE ALARMS - EAST
-module "sqs_alarms_east" {
-  source = "./modules/sqs_alarms"
+######################################
+# SQS QUEUE ALARMS - EAST REGION
+######################################
 
-  providers = {
-    aws = aws.useast1
-  }
+module "sqs_alarms_east" {
+  source    = "./modules/sqs_alarms"
+  providers = { aws = aws.useast1 }
 
   for_each = {
     for queue in var.sqs_queues_east : queue.name => queue
+  }
+
+  queue_name                   = each.value.name
+  visible_threshold            = each.value.visible_threshold
+  oldest_message_age_threshold = each.value.oldest_message_age_threshold
+}
+
+
+
+
+
+######################################
+# SQS QUEUE ALARMS - APAC REGION
+######################################
+
+module "sqs_alarms_apac" {
+  source    = "./modules/sqs_alarms"
+  providers = { aws = aws.apac }
+
+  for_each = {
+    for queue in var.sqs_queues_apac : queue.name => queue
   }
 
   queue_name                   = each.value.name
--- outputs.tf
+++ outputs.tf
@@ -1,42 +1,137 @@
-output "cpu_alarm_names" {
+# ---------------------
+# RDS Reader Alarms
+# ---------------------
+
+output "cpu_alarm_names_west" {
   value = {
-    for key, mod in module.rds_alarms :
+    for key, mod in module.rds_alarms_west :
     key => mod.cpu_alarm_name
   }
 }
 
-output "connection_alarm_names" {
+output "connection_alarm_names_west" {
   value = {
-    for key, mod in module.rds_alarms :
+    for key, mod in module.rds_alarms_west :
     key => mod.connection_alarm_name
   }
 }
 
-output "cpu_writer_alarm_names" {
+output "cpu_alarm_names_east" {
   value = {
-    for key, mod in module.rds_writer_alarms :
+    for key, mod in module.rds_alarms_east :
     key => mod.cpu_alarm_name
   }
 }
 
-output "connection_writer_alarm_names" {
+output "connection_alarm_names_east" {
   value = {
-    for key, mod in module.rds_writer_alarms :
+    for key, mod in module.rds_alarms_east :
     key => mod.connection_alarm_name
   }
 }
 
-output "visible_messages_alarm_names" {
+output "cpu_alarm_names_apac" {
   value = {
-    for key, mod in module.sqs_alarms :
+    for key, mod in module.rds_alarms_apac :
+    key => mod.cpu_alarm_name
+  }
+}
+
+output "connection_alarm_names_apac" {
+  value = {
+    for key, mod in module.rds_alarms_apac :
+    key => mod.connection_alarm_name
+  }
+}
+
+# ---------------------
+# RDS Writer Alarms
+# ---------------------
+
+output "cpu_writer_alarm_names_west" {
+  value = {
+    for key, mod in module.rds_writer_alarms_west :
+    key => mod.cpu_alarm_name
+  }
+}
+
+output "connection_writer_alarm_names_west" {
+  value = {
+    for key, mod in module.rds_writer_alarms_west :
+    key => mod.connection_alarm_name
+  }
+}
+
+output "cpu_writer_alarm_names_east" {
+  value = {
+    for key, mod in module.rds_writer_alarms_east :
+    key => mod.cpu_alarm_name
+  }
+}
+
+output "connection_writer_alarm_names_east" {
+  value = {
+    for key, mod in module.rds_writer_alarms_east :
+    key => mod.connection_alarm_name
+  }
+}
+
+output "cpu_writer_alarm_names_apac" {
+  value = {
+    for key, mod in module.rds_writer_alarms_apac :
+    key => mod.cpu_alarm_name
+  }
+}
+
+output "connection_writer_alarm_names_apac" {
+  value = {
+    for key, mod in module.rds_writer_alarms_apac :
+    key => mod.connection_alarm_name
+  }
+}
+
+# ---------------------
+# SQS Alarms
+# ---------------------
+
+output "visible_messages_alarm_names_west" {
+  value = {
+    for key, mod in module.sqs_alarms_west :
     key => mod.visible_messages_alarm_name
   }
 }
 
-output "oldest_message_alarm_names" {
+output "oldest_message_alarm_names_west" {
   value = {
-    for key, mod in module.sqs_alarms :
+    for key, mod in module.sqs_alarms_west :
     key => mod.oldest_message_alarm_name
   }
 }
 
+output "visible_messages_alarm_names_east" {
+  value = {
+    for key, mod in module.sqs_alarms_east :
+    key => mod.visible_messages_alarm_name
+  }
+}
+
+output "oldest_message_alarm_names_east" {
+  value = {
+    for key, mod in module.sqs_alarms_east :
+    key => mod.oldest_message_alarm_name
+  }
+}
+
+output "visible_messages_alarm_names_apac" {
+  value = {
+    for key, mod in module.sqs_alarms_apac :
+    key => mod.visible_messages_alarm_name
+  }
+}
+
+output "oldest_message_alarm_names_apac" {
+  value = {
+    for key, mod in module.sqs_alarms_apac :
+    key => mod.oldest_message_alarm_name
+  }
+}
